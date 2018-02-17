6a6c9e0be2f5c97319dde9b635868f328c126670
Chris Lengerich <chris.lengerich@gmail.com>
2018-02-17 15:55:58 -0800

    Add endpoint to post an image. Query as 'curl -F "image=@/Users/thor/Downloads/calvin_and_hobbes.jpg" localhost:5000/image'

--- image_server/server.py
+++ image_server/server.py
@@ -1,7 +1,24 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
+
+# TODO: Add security (shared secret key)
+
+# POST an image.
+@app.route('/image', methods=['POST'])
+def image():
+    print request.files
+    print request
+    if 'image' in request.files:
+        file = request.files['image']
+        file_path = 'static/recent.jpg'
+        file.save(file_path)
+        return jsonify({'path': 'recent.jpg'})
+    else:
+        return jsonify({'path': ''})
 
 @app.route('/')
 def index():
