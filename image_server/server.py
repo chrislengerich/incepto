b6d7d558a62143b5a7ba6c8dfbbab09992d21e4b
Chris Lengerich <chris.lengerich@gmail.com>
2018-02-17 16:23:41 -0800

    Add preshared key for simple security.

--- image_server/server.py
+++ image_server/server.py
@@ -1,17 +1,37 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from flask import request
+from flask import request, Response
+from functools import wraps
 
 app = Flask(__name__)
 
-# TODO: Add security (shared secret key)
+# Simple security: pre-shared key.
+API_KEY = "b09c4de1043f8ad0815d"
+
+def check_auth(key):
+    print key
+    return key == API_KEY
+
+def authenticate():
+    """Sends 401 response"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper API key', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not 'key' in request.args or not check_auth(request.args['key']):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 # POST an image.
 @app.route('/image', methods=['POST'])
+@requires_auth
 def image():
-    print request.files
-    print request
     if 'image' in request.files:
         file = request.files['image']
         file_path = 'static/recent.jpg'
@@ -21,7 +41,10 @@ def image():
         return jsonify({'path': ''})
 
 @app.route('/')
+@requires_auth
 def index():
+    if 'key' not in request.args and request.args['key'] != API_KEY:
+        return render_template('')
     return render_template('index.html')
 
 if __name__ == '__main__':
